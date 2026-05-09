8df434ecc1811dbabde1f935eacb86d124057212
Justus Springer <justusspringer@gmx.de>
2026-05-09 18:14:17 +0100

    finish Birational file

--- Mathlib/AlgebraicGeometry/Birational/BirationalMap.lean
+++ Mathlib/AlgebraicGeometry/Birational/BirationalMap.lean
@@ -5,180 +5,39 @@ Authors: Justus Springer
 -/
 module
 
-public import Mathlib.AlgebraicGeometry.AffineSpace
-public import Mathlib.AlgebraicGeometry.Birational.RationalMap
+public import Mathlib.AlgebraicGeometry.Birational.Birational
+public import Mathlib.AlgebraicGeometry.Birational.Composition
 /-!
 
 # Birational maps between schemes
 
-TODO
+## TODO
 
 -/
 
 @[expose] public section
 
-universe u v
+universe u
 
-open CategoryTheory hiding Quotient
+open CategoryTheory
 
 namespace AlgebraicGeometry
 
 namespace Scheme
 
-variable {X Y Z S : Scheme.{u}}
+structure BirationalMap (X Y : Scheme.{u}) [IrreducibleSpace X] [IrreducibleSpace Y] where
+  hom : X ⤏ Y
+  [isDominant_hom : hom.IsDominant]
+  inv : Y ⤏ X
+  [isDominant_inv : inv.IsDominant]
+  hom_comp_inv_id : hom.comp inv = .id X
+  inv_comp_hom_id : inv.comp hom = .id Y
 
-/--
-A partial isomorphism from `X` to `Y` (`X.BipartialMap Y`) is... TODO
--/
-structure PartialIso (X Y : Scheme.{u}) where
-  /-- The source of definition of a partial iso. -/
-  source : X.Opens
-  dense_source : Dense (source : Set X)
-  /-- The target of a partial iso. -/
-  target : Y.Opens
-  dense_target : Dense (target : Set Y)
-  /-- The underlying isomorphism of a partial iso. -/
-  iso : source.toScheme ≅ target.toScheme
+def PartialIso.toBirationalMap {X Y : Scheme.{u}} [IrreducibleSpace X] [IrreducibleSpace Y]
+    (f : X.PartialIso Y) : X.BirationalMap Y where
+  hom := f.toRationalMap
 
-namespace PartialIso
 
-variable (S) in
-abbrev IsOver (f : X.PartialIso Y) [X.Over S] [Y.Over S] : Prop :=
-  f.iso.hom.IsOver S
-
-lemma ext_iff (f g : X.PartialIso Y) :
-    f = g ↔ ∃ (e : f.source = g.source) (e' : g.target = f.target),
-      f.iso = X.isoOfEq e ≪≫ g.iso ≪≫ Y.isoOfEq e' := by
-  constructor
-  · rintro rfl
-    simp
-  · obtain ⟨U₁, hU₁, U₂, hU₂, f⟩ := f
-    obtain ⟨V₁, hV₁, V₂, hU₂, g⟩ := g
-    simp only [forall_exists_index]
-    rintro rfl rfl e
-    simpa using e
-
-@[ext]
-lemma ext (f g : X.PartialIso Y) (e : f.source = g.source) (e' : g.target = f.target)
-    (H : f.iso = X.isoOfEq e ≪≫ g.iso ≪≫ Y.isoOfEq e') : f = g := by
-  rw [ext_iff]
-  exact ⟨e, e', H⟩
-
-variable (X) in
-@[simps]
-def refl : X.PartialIso X where
-  source := ⊤
-  dense_source := dense_univ
-  target := ⊤
-  dense_target := dense_univ
-  iso := Iso.refl _
-
-instance [X.Over S] : (refl X).IsOver S := by
-  simp only [Hom.isOver_iff, refl_source, refl_target, refl_iso, Iso.refl_hom, Category.id_comp]
-  rfl
-
-@[symm, simps]
-def symm (f : X.PartialIso Y) : Y.PartialIso X where
-  source := f.target
-  dense_source := f.dense_target
-  target := f.source
-  dense_target := f.dense_source
-  iso := f.iso.symm
-
-set_option backward.isDefEq.respectTransparency false in
-instance [X.Over S] [Y.Over S] (f : X.PartialIso Y) [f.IsOver S] :
-    f.symm.IsOver S :=
-  OverClass.instHomIsOverInvOfHom S
-
-@[simps]
-noncomputable def trans' (f : X.PartialIso Y) (g : Y.PartialIso Z) (e : f.target = g.source) :
-    X.PartialIso Z where
-  source := f.source
-  dense_source := f.dense_source
-  target := g.target
-  dense_target := g.dense_target
-  iso := f.iso ≪≫ Y.isoOfEq e ≪≫ g.iso
-
-@[simps]
-noncomputable def restrictSource (f : X.PartialIso Y) (U : Opens X) (hU : Dense (U : Set X))
-    (hU' : U ≤ f.source) : X.PartialIso Y where
-  source := U
-  dense_source := hU
-  target := f.target.ι ''ᵁ f.iso.hom ''ᵁ f.source.ι ⁻¹ᵁ U
-  dense_target :=
-    have := PartialMap.Opens.isDominant_ι f.dense_target
-    f.target.ι.denseRange.dense_image f.target.ι.continuous <|
-      f.iso.hom.denseRange.dense_image f.iso.hom.continuous <|
-        hU.preimage f.source.ι.isOpenEmbedding.isOpenMap
-  iso := (Opens.isoOfLE hU').symm ≪≫
-    (f.iso.hom.isoImage (f.source.ι ⁻¹ᵁ U)) ≪≫
-    (f.target.ι.isoImage (f.iso.hom ''ᵁ f.source.ι ⁻¹ᵁ U))
-
-noncomputable def restrictTarget (f : X.PartialIso Y) (U : Opens Y) (hU : Dense (U : Set Y))
-    (hU' : U ≤ f.target) : X.PartialIso Y where
-  source := sorry
-  dense_source := sorry
-  target := U
-  dense_target := hU
-  iso := sorry
- 
-@[simps]
-def toPartialMap (f : X.PartialIso Y) : X.PartialMap Y where
-  domain := f.source
-  dense_domain := f.dense_source
-  hom := f.iso.hom ≫ f.target.ι
-
-abbrev toRationalMap (f : X.PartialIso Y) : X ⤏ Y := f.toPartialMap.toRationalMap
-
-@[simps]
-noncomputable def _root_.CategoryTheory.Iso.toPartialIso (f : X ≅ Y) : X.PartialIso Y where
-  source := ⊤
-  dense_source := dense_univ
-  target := ⊤
-  dense_target := dense_univ
-  iso := X.topIso ≪≫ f ≪≫ Y.topIso.symm
-
-end PartialIso
-
-variable (X Y) in
-def Birational : Prop := Nonempty (PartialIso X Y)
-
-noncomputable def Birational.partialIso (h : Birational X Y) : PartialIso X Y :=
-  Classical.choice h
-
-variable (X) in
-lemma Birational.refl : Birational X X :=
-  ⟨.refl X⟩
-
-lemma Birational.symm (h : Birational X Y) : Birational Y X :=
-  ⟨h.partialIso.symm⟩
-
-variable (X Y S) in
-def BirationalOver [X.Over S] [Y.Over S] : Prop :=
-  ∃ f : PartialIso X Y, f.IsOver S
-
-noncomputable def BirationalOver.partialIso [X.Over S] [Y.Over S] (h : BirationalOver X Y S) :=
-  h.choose
-
-instance BirationalOver.partialIso_isOver [X.Over S] [Y.Over S] (h : BirationalOver X Y S) :
-    h.partialIso.IsOver S :=
-  h.choose_spec
-
-variable (X) in
-lemma BirationalOver.refl [X.Over S] : BirationalOver X X S :=
-  ⟨.refl X, inferInstance⟩
-
-lemma BirationalOver.symm [X.Over S] [Y.Over S] (h : BirationalOver X Y S) :
-    BirationalOver Y X S :=
-  ⟨h.partialIso.symm, inferInstance⟩
-
-variable (X S) in
-@[mk_iff]
-class IsRationalOver [X.Over S] : Prop where
-  exists_birationalOver_affineSpace : ∃ n, BirationalOver X 𝔸(Fin n; S) S
-
-instance (n : ℕ) : IsRationalOver 𝔸(Fin n; S) S where
-  exists_birationalOver_affineSpace := ⟨n, .refl _⟩
 
 end Scheme
 
--- Mathlib/AlgebraicGeometry/Birational/Category.lean
+++ Mathlib/AlgebraicGeometry/Birational/Category.lean
@@ -5,7 +5,7 @@ Authors: Justus Springer
 -/
 module
 
-public import Mathlib.AlgebraicGeometry.Birational.Dominant
+public import Mathlib.AlgebraicGeometry.Birational.Composition
 /-!
 
 # The category of irreducible schemes with dominant rational maps
@@ -25,92 +25,6 @@ namespace AlgebraicGeometry
 variable {X Y Z : Scheme.{u}}
 
 namespace Scheme
-
-namespace PartialMap
-
-variable [PreirreducibleSpace X] [Nonempty Y]
-
-@[simps]
-noncomputable def comp (f : X.PartialMap Y) [IsDominant f.hom] (g : Y.PartialMap Z) :
-    X.PartialMap Z where
-  domain := f.domain.ι ''ᵁ f.hom ⁻¹ᵁ g.domain
-  dense_domain := by
-    obtain ⟨_, hx, ⟨x, rfl⟩⟩ :=
-      f.hom.denseRange.inter_open_nonempty _ g.domain.2 g.dense_domain.nonempty
-    exact (f.domain.ι ''ᵁ f.hom ⁻¹ᵁ g.domain).2.dense ⟨x.1, ⟨x, hx, rfl⟩⟩
-  hom := (f.domain.ι.isoImage _).inv ≫ (f.hom ∣_ g.domain) ≫ g.hom
-
-lemma comp_restrict_left (f : X.PartialMap Y) [IsDominant f.hom] (U : X.Opens)
-    (hU : Dense (U : Set X)) (hU' : U ≤ f.domain) (g : Y.PartialMap Z) :
-    (f.restrict U hU hU').comp g = (f.comp g).restrict (f.domain.ι ''ᵁ f.hom ⁻¹ᵁ g.domain ⊓ U)
-      ((f.comp g).dense_domain.inter_of_isOpen_right hU U.2) inf_le_left := by
-  sorry
-
-lemma comp_restrict_right (f : X.PartialMap Y) [IsDominant f.hom] (g : Y.PartialMap Z)
-    (V : Y.Opens) (hV : Dense (V : Set Y)) (hV' : V ≤ g.domain) :
-    f.comp (g.restrict V hV hV') = (f.comp g).restrict
-      (f.domain.ι ''ᵁ (f.hom ⁻¹ᵁ V)) (by 
-        apply (f.domain.ι ''ᵁ f.hom ⁻¹ᵁ V).2.dense
-        simp
-        sorry
-        ) sorry := by
-  sorry
-
-lemma comp_equiv_of_equiv_left (f₁ f₂ : X.PartialMap Y) [IsDominant f₁.hom] [IsDominant f₂.hom]
-    (h : f₁.equiv f₂) (g : Y.PartialMap Z) :
-    (f₁.comp g).equiv (f₂.comp g) := sorry
-
-lemma comp_equiv_of_equiv_right (f : X.PartialMap Y) [IsDominant f.hom] (g₁ g₂ : Y.PartialMap Z)
-    (h : g₁.equiv g₂) : (f.comp g₁).equiv (f.comp g₂) := sorry
-
-lemma comp_equiv_of_equiv (f₁ f₂ : X.PartialMap Y) [IsDominant f₁.hom] [IsDominant f₂.hom]
-    (hf : f₁.equiv f₂) (g₁ g₂ : Y.PartialMap Z) (hg : g₁.equiv g₂) :
-    (f₁.comp g₁).equiv (f₂.comp g₂) :=
-  equivalence_rel.trans (comp_equiv_of_equiv_left _ _ hf _) (comp_equiv_of_equiv_right _ _ _ hg)
-
-lemma comp_toPartialMap (f : X.PartialMap Y) [IsDominant f.hom] (g : Y ⟶ Z) :
-    f.comp g.toPartialMap = f.compHom g := by
-  ext x
-  · constructor
-    · intro ⟨⟨_, h₁⟩, _, h₂⟩
-      exact h₂ ▸ h₁
-    · intro hx
-      sorry
-  · simp
-    sorry
-
-variable (X) in
-protected abbrev id : X.PartialMap X := (𝟙 X : X ⟶ X).toPartialMap
-
-lemma comp_id [PreirreducibleSpace X] [Nonempty Y]
-    (f : X.PartialMap Y) [IsDominant f.hom] : f.comp (PartialMap.id Y) = f := by
-  simp [PartialMap.comp_toPartialMap]
-
-lemma id_comp [IrreducibleSpace X] (f : X.PartialMap Y) :
-    (PartialMap.id X).comp f = f := by
-  ext x
-  · sorry
-  · simp
-    sorry
-
-end PartialMap
-
-noncomputable def RationalMap.comp [PreirreducibleSpace X] [Nonempty Y]
-    (f : X ⤏ Y) [f.IsDominant] (g : Y ⤏ Z) : X ⤏ Z :=
-  Quotient.liftOn g (PartialMap.toRationalMap ∘ f.dominantRep.comp) <| fun _ _ h ↦ by
-    rw [Function.comp_apply, Function.comp_apply, PartialMap.toRationalMap_eq_iff]
-    exact PartialMap.comp_equiv_of_equiv_right _ _ _ h
-
-instance [PreirreducibleSpace X] [Nonempty Y] (f : X ⤏ Y) [f.IsDominant] (g : Y ⤏ Z)
-    [g.IsDominant] : (f.comp g).IsDominant := sorry
-
-lemma RationalMap.comp_toRationalMap [PreirreducibleSpace X] [Nonempty Y] (f : X ⤏ Y)
-    [f.IsDominant] (g : Y.PartialMap Z) :
-    f.comp g.toRationalMap = (f.dominantRep.comp g).toRationalMap :=
-  rfl
-
-variable (X) in
-abbrev RationalMap.id : X ⤏ X := (𝟙 X : X ⟶ X).toRationalMap
 
 structure BirationalScheme where
   private mk ::
